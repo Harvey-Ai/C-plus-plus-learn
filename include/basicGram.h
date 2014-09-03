9219afc6d3ba613ef2e63f440585c23ed8652475
aihongwei <ufo008ahw@163.com>
2014-09-03 12:12:29 +0800

    add #if test

--- include/basicGram.h
+++ include/basicGram.h
@@ -3,9 +3,13 @@
 using namespace std;
 
 
-/* Learning tips : be careful of the type conversion in C.
+/* Learning tips
  *
- * 1. In the "? :" operation, the result type is depend on both last two operands.
+ * 1. Be careful of the type conversion in C. in the "? :" operation, the result type is
+ * depend on both last two operands.
+ * 2. View the typedef as a variable declaration.
+ * 3. The #if condition compilation is calculated in the pre_process part, so ordinary variable
+ * can't work in the #if, even the sizeof operator.
  *
  */
 
@@ -15,6 +19,7 @@ class basicGram: public rawClass
 	private:
 
 	public:
+		typedef int intType;
 		void doTest();
 };
 
@@ -26,4 +31,23 @@ void basicGram::doTest()
 			<< (sizeof(1 > 0 ? 1 : 0) == sizeof(int) ? "YES" : "NO") << endl;
 	cout << "sizeof(1 > 0 ? 1 : 0.0) = " << sizeof(1 > 0 ? 1 : 0.0) << ", " \
 			<< (sizeof(1 > 0 ? 1 : 0.0) == sizeof(double) ? "YES" : "NO") << endl;
+
+	basicGram::intType intValue;
+	cout << "basicGram::intType intValue, sizeof(intValue) = " << sizeof(intValue) << ", " \
+			<< (sizeof(int) == sizeof(intValue) ? "YES" : "NO") << endl;
+
+	int macroCond = 1;
+#if macroCond
+	cout << "macroCond work, NO" << endl;
+#else
+	cout << "macroCond do not work. YES" << endl;
+#endif
+
+#define definedMarcoCond 1
+#if definedMarcoCond
+	cout << "definedMarcoCond work, YES" << endl;
+#else
+	cout << "definedMarcoCond do not work. NO" << endl;
+#endif
+
 }
